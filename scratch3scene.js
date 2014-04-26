6a6e162b80a7dba824bcf20637ae691d72018050
root <root@irene.(none)>
2014-04-26 18:41:27 -0400

    dancing desk

--- scratch3scene.js
+++ scratch3scene.js
@@ -4,26 +4,52 @@
   Date: July 2013 (three.js v59dev)
  */
 
-// MAIN
+// imports
+//var table_gen = require('/table_generator');
 
 // standard global variables
 var container, scene, camera, renderer, controls, stats;
 var keyboard = new THREEx.KeyboardState();
-var clock = new THREE.Clock();
+
 
 // custom global variables
-var mesh;
+var array_table_mesh = [];
+var sphere_mesh;
+var time_prev;
 
-init();
-animate();
+// constant global varibles
+var INT_TABLE_NUM;
+
+// start
+main();
+
+
+
+//===========================================
+// Main function
+//===========================================
+function main(){
+  inputs();
+  init();
+  animate();
+}
+
 
 // FUNCTIONS    
+function inputs(){
+// This function runs and gathers inputs on how many tables to apply
+// Will be extended to ask for more information regarding furnature
+// to be concidered in a room, desk/tables/lamps ect....
+  INT_TABLE_NUM = prompt("Number of Desk","5");
+}
+
 function init() 
 {
 
+  // TIME
+  time_prev = 
   // SCENE
   scene = new THREE.Scene();
-
   // CAMERA
   var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
   var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
@@ -31,7 +57,6 @@ function init()
   scene.add(camera);
   camera.position.set(0,150,400);
   camera.lookAt(scene.position);  
-
   // RENDERER
   if ( Detector.webgl )
     renderer = new THREE.WebGLRenderer( {antialias:true} );
@@ -40,25 +65,21 @@ function init()
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
   container = document.getElementById( 'ThreeJS' );
   container.appendChild( renderer.domElement );
-
   // EVENTS
   THREEx.WindowResize(renderer, camera);
   THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
-
   // CONTROLS
   controls = new THREE.OrbitControls( camera, renderer.domElement );
-
   // STATS
   stats = new Stats();
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.bottom = '0px';
   stats.domElement.style.zIndex = 100;
   container.appendChild( stats.domElement );
-
   // LIGHT
   var light = new THREE.PointLight(0xffffff);
   light.position.set(100,250,100);
-  scene.add(light);
+  scene.add(light)
 
   /*
   // FLOOR
@@ -79,49 +100,54 @@ function init()
   var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
   scene.add(skyBox);
   
-  ////////////
-  // CUSTOM //
-  ////////////
-  
-  // Sphere Mesh
-  var geometry = new THREE.SphereGeometry( 30, 32, 16 );
+  setup_objs('../output/tween/tbl017.obj', '../textures/UV_Grid_Sm.jpg ',array_table_mesh, INT_TABLE_NUM);
+  console.log(array_table_mesh);
+
+  //// TEXTURE OF DESK
+  //var manager = new THREE.LoadingManager();
+  //manager.onProgress = function ( item, loaded, total ) {
+  //  console.log( item, loaded, total );
+  //};
+
+  //var texture = new THREE.Texture();
+  //var loader = new THREE.ImageLoader( manager );
+  //loader.load( '../textures/UV_Grid_Sm.jpg', function ( image ) {
+  //  texture.image = image;
+  //  texture.needsUpdate = true;
+
+  //} );
+
+  //// MESH OBJ LOADER
+  //var loader = new THREE.OBJLoader( manager );
+  //loader.load( '../output/tween/tbl017.obj', function ( object ) {
+  //  object.traverse( function ( child ) {
+  //    if ( child instanceof THREE.Mesh ) {
+  //      child.material.map = texture;
+  //    }
+  //  } );
+  //  mesh_table = object;
+  //  object.position.y = 0;
+  //  object.scale.x = .01;
+  //  object.scale.y = .01;
+  //  object.scale.z = .01;
+  //  object.rotation.x += 0.5*Math.PI;
+  //  object.rotation.x += 0.5*Math.PI;
+  //  object.rotation.x += 0.5*Math.PI;
+  //  scene.add( mesh_table );
+  //  animate();
+  //});
+
+
+  // SPHERE
+  geometry = new THREE.SphereGeometry( 3, 32, 16 );
   var material = new THREE.MeshLambertMaterial( { color: 0x000088 } );
-  mesh = new THREE.Mesh( geometry, material );
-  mesh.position.set(40,40,40);
-  scene.add(mesh);
-  
-  // texture
-  var manager = new THREE.LoadingManager();
-  manager.onProgress = function ( item, loaded, total ) {
-    console.log( item, loaded, total );
-  };
+  sphere_mesh = new THREE.Mesh( geometry, material );
+  sphere_mesh.position.set(40,40,40);
+  scene.add(sphere_mesh);
 
-  var texture = new THREE.Texture();
-  var loader = new THREE.ImageLoader( manager );
-  loader.load( '../textures/UV_Grid_Sm.jpg', function ( image ) {
-    texture.image = image;
-    texture.needsUpdate = true;
-
-  } );
-
-  var loader = new THREE.OBJLoader( manager );
-  loader.load( '../output/tween/tbl017.obj', function ( object ) {
-    object.traverse( function ( child ) {
-      if ( child instanceof THREE.Mesh ) {
-        child.material.map = texture;
-      }
-    } );
-    object.position.y = 0;
-    object.scale.x = .01;
-    object.scale.y = .01;
-    object.scale.z = .01;
-
-    scene.add( object );
-
-  });
-
+  // Background
   var axes = new THREE.AxisHelper(50);
-  axes.position = mesh.position;
+  axes.position = sphere_mesh.position;
   scene.add(axes);
   
   var gridXZ = new THREE.GridHelper(100, 10);
@@ -147,12 +173,11 @@ function init()
   var direction = new THREE.Vector3().subVectors(terminus, origin).normalize();
   var arrow = new THREE.ArrowHelper(direction, origin, 50, 0x884400);
   scene.add(arrow);
-  
-  
 }
 
 function animate() 
 {
+  // Keeping track of time
   requestAnimationFrame( animate );
   render();   
   update();
@@ -160,11 +185,32 @@ function animate()
 
 function update()
 {
-  if ( keyboard.pressed("z") ) 
+  if ( keyboard.pressed("d") ) 
   { // do something   
-    var t = clock.getElapsedTime();
-    console.log(t);
+    sphere_mesh.position.x += .05;
+    console.log(sphere_mesh.position.x, sphere_mesh.position.y, sphere_mesh.position.z);
   }
+  if ( keyboard.pressed("w") ) 
+  { // do something   
+    sphere_mesh.position.y += .05;
+    console.log(sphere_mesh.position.x, sphere_mesh.position.y, sphere_mesh.position.z);
+  }
+  if ( keyboard.pressed("s") ) 
+  { // do something   
+    sphere_mesh.position.z += .05;
+    console.log(sphere_mesh.position.x, sphere_mesh.position.y, sphere_mesh.position.z);
+  }
+  if ( keyboard.pressed("r") ) 
+  { // do something   
+    for(var i = 0; i <array_table_mesh.length; i++){
+      
+      if(array_table_mesh[i]){
+        array_table_mesh[i].position.x =Math.floor(Math.random()*200);
+        array_table_mesh[i].position.z =Math.floor(Math.random()*200);
+      }
+    }
+  }
+  
   
   controls.update();
   stats.update();
@@ -173,4 +219,54 @@ function update()
 function render() 
 {
   renderer.render( scene, camera );
+}
+
+function setup_objs(str_obj, str_texture ,array_mesh, int_copies){
+
+  var manager = new THREE.LoadingManager();
+  manager.onProgress = function ( item, loaded, total ) {
+    console.log( item, loaded, total );
+  };
+
+  var texture = new THREE.Texture();
+  var loader = new THREE.ImageLoader( manager );
+  loader.load( str_texture, function ( image ) {
+    texture.image = image;
+    texture.needsUpdate = true;
+  } );
+
+
+  var int_offset = 20;
+  var int_x = 0;
+
+  // load in meshes
+  for(var i = 0 ; i < int_copies; i++ ){
+
+    var loader = new THREE.OBJLoader( manager );
+    loader.load( str_obj, function ( object ) {
+      object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+          child.material.map = texture;
+        }
+      } );
+      
+      // add to array
+      array_mesh.push(object);
+      
+      // roations to table
+      object.scale.x = .01;
+      object.scale.y = .01;
+      object.scale.z = .01;
+      object.rotation.x += 1.5*Math.PI;
+
+      // offsets
+      object.position.x = int_x*int_offset;
+      int_x++;
+
+      // Adding to scene
+      scene.add(array_mesh[array_mesh.length -1]);
+      animate();
+
+    });
+  } //for
 }
